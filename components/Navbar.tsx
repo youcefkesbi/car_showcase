467ad29ba7617599edf197b9018d7d6016195b4f
Youcef Kesbi <ykesbi@gmail.com>
2025-03-19 18:36:50 +0300

    Creating navbar and footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10'>
+        Navbar
+    </header>
+  )
+}
+
+export default Navbar
