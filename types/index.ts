ce322f13c4f5401cf21c75d9e802b15bab99cc74
Youcef Kesbi <ykesbi@gmail.com>
2025-03-22 19:14:38 +0300

    update

--- types/index.ts
+++ types/index.ts
@@ -13,15 +13,15 @@ export interface SearchManufacturerProps {
 }
 
 export interface CarProps {
-    city_mpg:23;
-    class:"compact car";
-    combination_mpg:24;
-    cylinders:4;
-    displacement:1.6;
-    drive:"fwd";
-    fuel_type:"gas";
-    highway_mpg:26;
-    make:"toyota";
-    model:"corolla";
-    transmission:"a";
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
 }
