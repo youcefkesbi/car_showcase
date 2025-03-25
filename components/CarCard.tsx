dfd6b0b21376c602458ea104d8e138b2eb6499c8
Youcef Kesbi <ykesbi@gmail.com>
2025-03-25 19:19:14 +0300

    updates

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from 'next/image';
-import useState from 'react';
+import { useState } from 'react';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,10 @@
 import { CustomButtonProps } from '@/types';
 import Image from 'next/image'
 
-const CustomButton = ( { title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+const CustomButton = ( { title, containerStyles, handleClick, btnType, textStyles,
+  rightIcon, isDisabled
+ }: CustomButtonProps) => {
+
   return (
     <button
       disabled={false}
@@ -11,9 +14,19 @@ const CustomButton = ( { title, containerStyles, handleClick, btnType }: CustomB
       className={`custom-btn ${containerStyles}`}
       onClick={() => {handleClick}}
     >
-      <span className='flex-1'>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image 
+            src={rightIcon}
+            alt='right icon'
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
     </button>
   )
 }
