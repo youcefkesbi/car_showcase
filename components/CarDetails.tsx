56b7c3eaebcbc7cf0cc81d4769513ab60893495e
Youcef Kesbi <ykesbi@gmail.com>
2025-03-25 21:44:08 +0300

    update

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -54,7 +54,13 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                     <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
                                 </div>
                                 <div className='flex gap-3'>
-                                    <div className='flex-1'>
+                                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100  rounded-lg'>
+                                    <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                                    </div>
+                                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100  rounded-lg'>
+                                    <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                                    </div>
+                                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100  rounded-lg'>
                                     <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
                                     </div>
                                 </div>
