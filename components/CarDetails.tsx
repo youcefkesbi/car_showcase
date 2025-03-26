244d53a4794d85d29f53cb225fa862aff65a655c
Youcef Kesbi <ykesbi@gmail.com>
2025-03-26 19:12:46 +0300

    update

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -66,7 +66,9 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                 </div>
                             </div>
                             <div className="flex-1 flex flex-col gap-2">
-                                <h2>{car.make} {car.model}</h2>
+                                <h2 className="font-semibold text-xl capitalize">
+                                    {car.make} {car.model}
+                                </h2>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
