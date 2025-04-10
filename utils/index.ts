71ed75805fd5c370a35450487d105253c7de0ce3
Youcef Kesbi <ykesbi@gmail.com>
2025-04-10 12:55:03 +0300

    .

--- utils/index.ts
+++ utils/index.ts
@@ -1,20 +1,20 @@
 import { CarProps } from "@/types";
 
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '6e50010ef7msh6ff32ae1ba26180p14b5acjsn91ebaddc628f',
-		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+// const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '6e50010ef7msh6ff32ae1ba26180p14b5acjsn91ebaddc628f',
+// 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
 }
 
 export async function fetchCars() {
