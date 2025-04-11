c4cbf806bedd2ea50b31e044e24d596c5bd6e8e6
Youcef Kesbi <ykesbi@gmail.com>
2025-04-11 12:19:34 +0300

    .

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,5 @@
 import { CarProps } from "@/types";
 
-// const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': '6e50010ef7msh6ff32ae1ba26180p14b5acjsn91ebaddc628f',
-// 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-}
-
 export async function fetchCars() {
     const headers = {
         'x-rapidapi-key': '6e50010ef7msh6ff32ae1ba26180p14b5acjsn91ebaddc628f',
@@ -24,7 +7,7 @@ export async function fetchCars() {
     }
 
     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
-        {headers: headers});
+        {headers: headers,});
 
     const result = await response.json();
 
@@ -46,15 +29,15 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     return rentalRatePerDay.toFixed(0);
   };
 
-//   export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-//     const url = new URL('https://cdn.imagin.studio/getimage');
-//     const { make, model, year } = car;
+  export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    // const url = new URL('https://cdn.imagin.studio/getimage');
+    // const { make, model, year } = car;
 
-//     url.searchParams.append('customer', 'hrjavascript-mastery');
-//     url.searchParams.append('make', make);
-//     url.searchParams.append('modelFamily', model);
-//     url.searchParams.append('zoomType', 'fullscreen');
-//     url.searchParams.append('angle', angle || 'front');
+    // url.searchParams.append('customer', 'hrjavascript-mastery');
+    // url.searchParams.append('make', make);
+    // url.searchParams.append('modelFamily', model);
+    // url.searchParams.append('zoomType', 'fullscreen');
+    // url.searchParams.append('angle', angle || 'front');
 
-//     return `${url}&year=${year}`;
-//   }
+    // return `${url}&year=${year}`;
+  }
